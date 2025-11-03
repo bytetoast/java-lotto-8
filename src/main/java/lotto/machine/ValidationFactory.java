436e87ee94353481219dcca67d0c1b12d9b82020
bytetoast <hvagdn@gmail.com>
2025-11-03 16:02:05 +0900

    feat(machine): add method to convert String to int

--- src/main/java/lotto/machine/ValidationFactory.java
+++ src/main/java/lotto/machine/ValidationFactory.java
@@ -1,4 +1,18 @@
 package lotto.machine;
 
 public class ValidationFactory {
+    public int convertToInt(String numberInput) {
+        int numberConverted = -1;
+        final String errorMessage = "번호는 1이상 45이하의 정수여야 합니다.";
+
+        try  {
+            numberConverted = Integer.parseInt(numberInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(errorMessage);
+        }
+        if (numberConverted < 1 || numberConverted > 45) {
+            throw new IllegalArgumentException(errorMessage);
+        }
+        return numberConverted;
+    }
 }
